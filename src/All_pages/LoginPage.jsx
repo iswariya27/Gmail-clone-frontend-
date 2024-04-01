2faebe5b1c98f68e5f87d8743d3d6b2c02dc9ac6
iswariya i <145429803+iswariya27@users.noreply.github.com>
2024-04-01 10:52:56 +0530

    gmail

--- src/All_pages/LoginPage.jsx
+++ src/All_pages/LoginPage.jsx
@@ -101,12 +101,12 @@ export default function SignIn() {
                 sx={{ mt: 3, mb: 2 }}>Sign In</Button>
               <Grid container>
                 <Grid item xs>
-                  <Link to="/forget" variant="body2">
+                  <Link href="/forget" variant="body2">
                     Forgot password?
                   </Link>
                 </Grid>
                 <Grid item>
-                  <Link to="/register" variant="body2">
+                  <Link href="/register" variant="body2">
                     {"Don't have an account? Sign Up"}
                   </Link>
                 </Grid>
